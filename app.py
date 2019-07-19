ea0101d9c5e078a06b2ae02757188500e11f6e0a
Nathan Pillai <natepill@gmail.com>
2019-07-19 16:28:35 -0700

    [edit] index html so that form reroutes to chart_visalization

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def landing_page():
 
+    # NOTE: CSS linkage is using absolute path, may cause issue during deployment
     return render_template('index.html')
 
+@app.route('/chart_visualization')
+def show_chart():
+    print("In chart chart_visualization")
+    return "HELLO WORLD!!!!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=33507)
