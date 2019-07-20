e0cd937c2b5e3cf3577943bed333562c5a1728b3
Nathan Pillai <natepill@gmail.com>
2019-07-19 17:22:56 -0700

    [added] obtaining and cleaning hashtag url parmeter

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def landing_page():
-
     # NOTE: CSS linkage is using absolute path, may cause issue during deployment
     return render_template('index.html')
 
-@app.route('/chart_visualization')
-def show_chart():
-    print("In chart chart_visualization")
-    return "HELLO WORLD!!!!"
+@app.route('/get_data')
+def start_streaming():
+    # Grab user input from url parameter and remove whitespace
+    hashtag = request.args.get('hashtag').replace(" ", "")
+
+
+    return hashtag
 
 if __name__ == "__main__":
     app.run(debug=True, port=33507)
