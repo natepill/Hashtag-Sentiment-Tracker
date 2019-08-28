d9ab3458ae8376657a3de3c317f602c97e17cc35
Nathan Pillai <natepill@gmail.com>
2019-08-28 10:31:03 -0700

    [added] display visualization route and bug notes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
+import requests
 from applied_ml import *
 import requests
 import time
-
 
 app = Flask(__name__)
 
@@ -19,14 +19,23 @@ def start_streaming():
     # Grab user input from url parameter and remove whitespace
     hashtag = request.args.get('hashtag').replace(" ", "")
 
-    # Histogram of emotion classifications
+    # Histogram of emotion classifications NOTE: currently just an array
     emotion_histogram = apply_ml(hashtag)
 
+    # TODO: Try seperating out the visualization to a different route?
+    # requests.post('http://localhost:33507/display_visualization', emotion_histogram)
+
     # NOTE: Currently throwing Tensorflow error when refreshing server after rendering html
-    # NOTE: Try making internal post request to a different route which renders the chart and
+    # NOTE: Try making internal post request  to a different route which renders the chart and
     # also send along the emotion_histogram
-    return emotion_histogram
+    return str(emotion_histogram)
     # return render_template('display_chart.html')
+
+@app.route('/display_visualization', methods=['GET', 'POST'])
+def display_chart():
+    pass
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=33507)
